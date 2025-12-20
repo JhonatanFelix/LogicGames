e4ae78ab14adcb3e1725b694fe85f31abaf3b915
Jhonatan Felix <jhonyrf@gmail.com>
2025-12-20 01:20:59 -0300

    Changed to names on list

--- RememberNames/FlaskRememberNames.py
+++ RememberNames/FlaskRememberNames.py
@@ -3,15 +3,17 @@ import random
 
 app = Flask(__name__)
 
+names = ["Jason", "Jennyfer", "Pablo", "Tyrony", "Plancton", "Roberson"]
+
 @app.route('/')
 def index():
-    return render_template('index.html', random_number=random_number)
+    return render_template('index.html', random_name=random_name)
 
 
-@app.route('/random_number')
-def random_number():
-    random_number = random.randint(1,100)
-    return jsonify(random_number=random_number)
+@app.route('/random_name')
+def random_name():
+    random_name = random.choice(names)
+    return jsonify(random_name=random_name)
 
 if __name__=='__main__':
     app.run(debug=True)
--- RememberNames/static/scripts.js
+++ RememberNames/static/scripts.js
@@ -2,14 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     var hint = document.getElementById('hint')
     var guessInput = document.getElementById('guess')
     var submitButton = document.getElementById('submit')
-    var randomNumber ;
+    var randomName ;
     var userGuesses = 0;
 
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200){
-            console.log(JSON.parse(xhr.responseText).random_number)
-            randomNumber = JSON.parse(xhr.responseText).random_number;
+            console.log(JSON.parse(xhr.responseText).random_name)
+            randomName = JSON.parse(xhr.responseText).random_name;
         }
     };
     xhr.open('GET', '/random_number', true);
@@ -19,18 +19,16 @@ document.addEventListener('DOMContentLoaded', function() {
         var userGuess = parseInt(guessInput.value);
         userGuesses += 1;
         
-        if (randomNumber !== undefined) {
-            if (userGuess < randomNumber) {
-                hint.textContent = 'Too Low! Try a higher number'
-            } else if (userGuess > randomNumber) {
-                hint.textContent = 'Too high! Try a lower number'
-            } else if (userGuess == randomNumber) {
-             hint.textContent = 'Congratulations! You guessed the correct number in ${userGuesses} guesses.'
+        if (randomName !== undefined) {
+            if (userGuess !== randomName) {
+                hint.textContent = 'Try another name'
+            } else if (userGuess == randomName) {
+             hint.textContent = 'Congratulations! You guessed the correct name in ${userGuesses} guesses.'
             } else {
             console.log('Error')
             }
         } else {
-            console.log('There is no defined random number')
+            console.log('There is no defined random name')
         }
     })
 })
