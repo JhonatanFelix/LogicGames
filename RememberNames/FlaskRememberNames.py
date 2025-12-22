755d0adbdece6b6ad5b58325db70dc5068664e54
Jhonatan Felix <jhonyrf@gmail.com>
2025-12-21 23:17:13 -0300

    added main functions

--- RememberNames/FlaskRememberNames.py
+++ RememberNames/FlaskRememberNames.py
@@ -7,17 +7,26 @@ names = ["Jason","Luis", "Jennyfer", "Pablo",
         "Tyrony", "Plancton", "Felipe", "Roberson", "Carlitos",
         "Pandero"]
 
-@app.route('/')
+@app.route("/")
 def index():
     selected = random.sample(names, 4)
     session["names"] = selected
-    return render_template('show.html', names=selected)
+    return render_template("show.html", names=selected)
 
 
-@app.route('/random_name')
-def random_name():
-    random_name = random.choice(names)
-    return jsonify(random_name=random_name)
+@app.route("/check", methods=["post"])
+def check():
+    user_input = request.form.get("guess")
+    user_names = user_input.split(",")
+
+    correct_names = session.get("names", [])
+    user_names = [n.strip() for n in user_names]
+
+    if sorted(user_names) == sorted(correct_names):
+        result = 'Correct!!!'
+    else:
+        result = f"Wrong, correct names were:{', '.join(correct_names)}"
+    return render_template("result.html", result=result)
 
 
 if __name__=='__main__':
